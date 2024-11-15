cb6241593780e6eb83c58cc55fd30babafdab4f4
Xingyao Wang <xingyao6@illinois.edu>
2024-11-15 05:08:58 +0000

    fix(swebench): handle error in eval_infer annd

--- evaluation/swe_bench/run_infer.py
+++ evaluation/swe_bench/run_infer.py
@@ -532,5 +532,5 @@ if __name__ == '__main__':
         output_file,
         args.eval_num_workers,
         process_instance,
-        timeout_seconds=60 * 60,  # 1 hour PER instance should be more than enough
+        timeout_seconds=120 * 60,  # 2 hour PER instance should be more than enough
     )
