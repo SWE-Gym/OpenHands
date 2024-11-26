160e99594e51440d354ed76ed7f30d35670b857e
Xingyao Wang <xingyao6@illinois.edu>
2024-11-26 18:35:10 +0000

    support multiple rounds for lite & verifed

--- scripts/rollout-eval-lite.sh
+++ scripts/rollout-eval-lite.sh
@@ -3,9 +3,13 @@
 export EXP_NAME="t0"  # use this to differentiate between different runs
 MODEL=$1
 NUM_WORKERS=${2:-64}
+N_RUNS=${3:-1}
 DATASET="princeton-nlp/SWE-bench_Lite"
 SPLIT="test"
-N_RUNS=1
+
+echo "MODEL: $MODEL"
+echo "NUM_WORKERS: $NUM_WORKERS"
+echo "N_RUNS: $N_RUNS"
 
 if [ -z "$ALLHANDS_API_KEY" ]; then
     echo "ALLHANDS_API_KEY is not set. Please set it and run the script again."
@@ -20,4 +24,4 @@ export EXP_NAME=$EXP_NAME
 EVAL_LIMIT=300
 MAX_ITER=100
 
-./evaluation/swe_bench/scripts/run_infer.sh $MODEL HEAD CodeActAgent $EVAL_LIMIT $MAX_ITER $NUM_WORKERS $DATASET $SPLIT
+./evaluation/swe_bench/scripts/run_infer.sh $MODEL HEAD CodeActAgent $EVAL_LIMIT $MAX_ITER $NUM_WORKERS $DATASET $SPLIT $N_RUNS
